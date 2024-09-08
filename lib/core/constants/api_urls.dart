601e058be7b9ec9e0843ebf9b64636f7b0303b94
WutthinanK <developer1@npappdesign.com>
2024-09-08 22:16:20 +0700

    update api

--- lib/core/constants/api_urls.dart
+++ lib/core/constants/api_urls.dart
@@ -1,5 +1,5 @@
 class ApiUrls {
-  static const baseUrl = 'http://192.168.0.101:8080/api/v1/';
+  static const baseUrl = 'http://192.168.1.133:8080/api/v1/';
   static const register = '${baseUrl}auth/register';
   static const login = '${baseUrl}auth/login';
   static const userProfile = '${baseUrl}users/profile';
